baa5ff0a91353eb5dea109d04fe124f0f5cd52a4
sledro <dan@sledro.com>
2018-02-11 21:26:18 +0000

    More tests added

--- src/main/java/Rate.java
+++ src/main/java/Rate.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 public class Rate {
 
 	public Rate(CarParkKind kind, BigDecimal normalRate, BigDecimal discountedRate, ArrayList<Period> discountPeriods, ArrayList<Period> normalPeriods) {
-	
+		//Check is positive of throw exception
+		//https://stackoverflow.com/questions/4164521/compare-if-bigdecimal-is-greater-than-zero
+		if (normalRate.compareTo(BigDecimal.ZERO) < 1) {
+            throw new IllegalArgumentException();
+        }
 	}
 }
--- src/test/java/HaydenDanielTestTask1.java
+++ src/test/java/HaydenDanielTestTask1.java
@@ -21,44 +21,89 @@ public class HaydenDanielTestTask1 {
 	Period discountPeriod = new Period(8,9);
 	discountPeriods.add(discountPeriod);
 	
-	Period normalPeriod = new Period(11,19);
+	Period normalPeriod = new Period(9,21);
 	normalPeriods.add(normalPeriod);
 	}
 	
 	//Test 1
+	//Check that STAFF is valid and constructor runs
+	@org.junit.Test
+    public void staffEnum(){
+        new Rate(CarParkKind.STAFF, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
+    }
+    
+	//Test 2
+	//Check that STUDENT is valid and constructor runs
+    @org.junit.Test
+    public void studentEnum(){
+        new Rate(CarParkKind.STUDENT, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
+    }
+    
+	//Test 3
+	//Check that MANAGEMENT is valid and constructor runs
+    @org.junit.Test
+    public void managementEnum(){
+        new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
+    }
+    
+	//Test 4
+	//Check that VISITOR is valid and constructor runs
+    @org.junit.Test
+    public void visitorEnum(){
+        new Rate(CarParkKind.VISITOR, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
+    }
+	
+	//Test 5
+	//Check that the normalRate > 0.
 	@org.junit.Test
     public void normalRateGraterThanZero(){
         new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
     }
     
-	//Test 2
-	@org.junit.Test
+	//Test 6
+	//Check that if the normal rate is negative, an IllegalArgumentException exception will be thrown.
+	@org.junit.Test (expected = IllegalArgumentException.class)
     public void normalRateLessThanZero(){
         new Rate(CarParkKind.MANAGEMENT, new BigDecimal(-1), new BigDecimal(1), discountPeriods, normalPeriods);
     }
+	
+	//Test 7
+	//Check that if the normal rate is = 0, an IllegalArgumentException exception will be thrown.
+	@org.junit.Test (expected = IllegalArgumentException.class)
+    public void normalRateEqualToZero(){
+        new Rate(CarParkKind.MANAGEMENT, new BigDecimal(0), new BigDecimal(1), discountPeriods, normalPeriods);
+    }
     
-	//Test 3
+	//Test 8
+	//Check discount rate > 0
 	@org.junit.Test
     public void discountRateGraterThanZero(){
         new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1), new BigDecimal(1), discountPeriods, normalPeriods);
     }
     
-	//Test 4
-	@org.junit.Test
+	//Test 9
+	@org.junit.Test (expected = IllegalArgumentException.class)
     public void discountRateLessThanZero(){
         new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1), new BigDecimal(-1), discountPeriods, normalPeriods);
     }
     
-    //Test 5
+    //Test 10
 	@org.junit.Test
     public void normalRateGreaterThanDiscountRate(){	
        new Rate(CarParkKind.MANAGEMENT, new BigDecimal(2),new BigDecimal(1),discountPeriods,normalPeriods);
     }
     
-    //Test 6
+    //Test 11
 	@org.junit.Test
     public void normalRateLessThanDiscountRate(){	
        new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1),new BigDecimal(2),discountPeriods,normalPeriods);
     }
+	
+    //Test 12
+	@org.junit.Test
+    public void normalRateEqualDiscountRate(){	
+       new Rate(CarParkKind.MANAGEMENT, new BigDecimal(1),new BigDecimal(1),discountPeriods,normalPeriods);
+    }
+
     
 }
